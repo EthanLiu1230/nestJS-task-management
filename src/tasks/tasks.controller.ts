a123097073d887b78768b77c2f057973a36b41a8
Ethan Liu <liuyuxuan1230@outlook.com>
2020-10-17 06:18:11 +0800

    taskFilter. [filter, includes, @Query() parser]

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -5,26 +5,16 @@ import {
   Get,
   Param,
   Patch,
-  Post,
+  Post, Query,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
-
-  @Get()
-  getAllTasks(): Task[] {
-    return this.tasksService.getAllTasks();
-    // nest.js will do jsonify etc. to translate it into http-response
-  }
-
-  @Get('/:id') // or ':id', they both work
-  getTaskById(@Param('id') id: string): Task {
-    return this.tasksService.getTaskById(id);
-  }
 
   // @Post()
   // createTask(@Body() body) {
@@ -37,9 +27,21 @@ export class TasksController {
     return this.tasksService.createTask(createTaskDto);
   }
 
-  @Delete('/:id')
-  deleteTask(@Param('id') id: string): void {
-    this.tasksService.deleteTask(id);
+  @Get()
+  getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+    // @Query parse '?status=OPEN&search=NestJS' in url into object
+    // and assign to the var it decorated
+
+    if (Object.keys(filterDto).length) {
+      return this.tasksService.getTasksWithFilters(filterDto);
+    }
+    return this.tasksService.getAllTasks();
+    // nest.js will do jsonify etc. to translate it into http-response
+  }
+
+  @Get('/:id') // or ':id', they both work
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id);
   }
 
   @Patch('/:id/status')
@@ -49,4 +51,10 @@ export class TasksController {
   ): Task {
     return this.tasksService.updateTaskStatus(id, status);
   }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id: string): void {
+    this.tasksService.deleteTask(id);
+  }
+
 }
