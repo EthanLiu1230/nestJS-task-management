9c059e1d86cc9ef6d4c8d5cc4b5483d5cc77600c
Ethan Liu <liuyuxuan1230@outlook.com>
2020-10-16 15:42:35 +0800

    1st api

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks() {
+    return this.tasksService.getAllTasks();
+    // nest.js will do jsonify etc. to translate it into http-response
+  }
 }
