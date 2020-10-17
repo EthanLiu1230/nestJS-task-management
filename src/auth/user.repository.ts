7aa125aa689066e9a0511499b9e99349b82d0bfa
Ethan Liu <liuyuxuan1230@outlook.com>
2020-10-18 07:19:07 +0800

    Install bcrypt. Gen unique-per-user salt on the fly to hash password before .save().

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -5,15 +5,19 @@ import {
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
+import { genSalt, hash } from 'bcrypt';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async signUp(authCredentialsDto: AuthCredentialsDto) {
     const { username, password } = authCredentialsDto;
 
+    const salt = await genSalt();
     const user = new User();
     Object.assign(user, {
-      username, password,
+      salt,
+      username,
+      password: await UserRepository.hashPassword(password, salt),
     });
 
     try { await user.save();} catch (error) {
@@ -23,5 +27,10 @@ export class UserRepository extends Repository<User> {
         throw new InternalServerErrorException();
       }
     }
+  }
+
+  private static async hashPassword(
+    password: string, salt: string): Promise<string> {
+    return hash(password, salt);
   }
 }
