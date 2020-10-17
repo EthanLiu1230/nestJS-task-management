80a12f9ff93b104ed8afe05b54c92edb7bc9f090
Ethan Liu <liuyuxuan1230@outlook.com>
2020-10-18 06:05:09 +0800

    sign-up.

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,7 +1,16 @@
 import { Entity, EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 
 @EntityRepository(User)
-export class UserRepository extends Repository<User>{
+export class UserRepository extends Repository<User> {
+  async signUp(authCredentialsDto: AuthCredentialsDto) {
+    const { username, password } = authCredentialsDto;
 
+    const user = new User();
+    Object.assign(user, {
+      username, password,
+    });
+    await user.save();
+  }
 }
