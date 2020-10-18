5440195c2462d25f2b9904cbac51d32adf58aa20
Ethan Liu <liuyuxuan1230@outlook.com>
2020-10-19 06:50:43 +0800

    @ManyToOne, @OneToMany, options: {eager: true}.

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,11 +1,12 @@
 import {
   BaseEntity,
   Column,
-  Entity,
+  Entity, OneToMany,
   PrimaryGeneratedColumn,
   Unique,
 } from 'typeorm';
 import { hash } from 'bcrypt';
+import { Task } from '../tasks/task.entity';
 
 @Entity()
 @Unique(['username'])
@@ -21,6 +22,9 @@ export class User extends BaseEntity {
 
   @Column()
   salt: string;
+
+  @OneToMany(type => Task, task => task.user, { eager: true })
+  tasks: Task[];
 
   async validatePassword(password: string): Promise<boolean> {
     const hashedPassword = await hash(password, this.salt);
